c82a899cb9742f0214d8d0aa47ad9862c5b9f4e1
Igor <igor.traydakalo@gmail.com>
2017-09-20 00:03:42 +0300

    return json from file

--- src/main/java/hello/ProzorroController.java
+++ src/main/java/hello/ProzorroController.java
@@ -3,19 +3,13 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
-@RestController
-@RequestMapping("/json")
+@Controller
 public class ProzorroController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/json")
-    String tender() {
-        return "json/tender.json";
-    }
-
-    @RequestMapping(value = "/documents", method = RequestMethod.GET, produces = "text/json")
-    String document() {
+    @RequestMapping(value = "/json", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getTender() {
         return "json/tender.json";
     }
 }
